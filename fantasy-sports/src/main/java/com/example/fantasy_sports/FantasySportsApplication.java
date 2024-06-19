b998ef40a01a26e1cffc5a6866f5df64f2c9afd0
neilpatelll <neil0patel@gmail.com>
2024-06-18 23:50:21 -0500

    Add files via upload

---
+++ fantasy-sports/src/main/java/com/example/fantasy_sports/FantasySportsApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantasy_sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasySportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasySportsApplication.class, args);
+	}
+
+}
